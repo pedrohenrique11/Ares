87cc0f617625e84a7c9cc61d8aeb35767ec42d7b
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2024-09-18 01:12:57 +0000

    refactor(api): invalid email error

--- api/src/controllers/user/register.ts
+++ api/src/controllers/user/register.ts
@@ -1,14 +1,22 @@
 import { Request, Response } from "express";
 import { RegisterService } from "../../services/register";
 import { UserPrismaRository } from "../../repositories/prisma/user-prisma-repository";
+import { EmailAlreadyExists } from "../../services/errors/email-already-exists";
 
-const userRepository = new UserPrismaRository
 
 export async function register(req: Request, res: Response) {
-    const registerService = new RegisterService(userRepository)
-
-    await registerService.execute(req, res)
+    try {
+        const userRepository = new UserPrismaRository
+        const registerService = new RegisterService(userRepository)
     
-    res.status(201).send('user created')
+        await registerService.execute(req, res)
+        
+        res.status(201).send('user created')
+    }
+    catch(err) {
+        if(err instanceof EmailAlreadyExists) {
+            res.status(401).send(err.message)
+        }
+    }
 
 }
