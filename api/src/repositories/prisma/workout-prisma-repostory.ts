30fb6ea13704d189682ea9fe4d92e89e557a4475
Pedro Henrique <pedrohenriqueb.dev@gmail.com>
2024-09-25 22:41:27 -0300

    feat(api): create prisma workout repostory and repository interface

---
+++ api/src/repositories/prisma/workout-prisma-repostory.ts
@@ -0,0 +1,37 @@
+import { Prisma, Workout } from "@prisma/client";
+import { WorkoutRepository } from "../worktout-repository";
+import prisma from "../../utils/prismaClient";
+
+export class WorkoutPrismaRepository implements WorkoutRepository {
+    async create(data: Prisma.WorkoutCreateInput) {
+        const workout = await prisma.workout.create({
+            data,
+        })
+        return workout
+    }
+    async delete(id: string) {
+        return await prisma.workout.delete({
+            where: {
+                id, 
+            }
+        })
+    }
+    async getCompletedWorkouts(isCompleted: boolean): Promise<Workout[]> {
+        const completedWorkouts = await prisma.workout.findMany({
+            where: {
+                isCompleted: true
+            }
+        })
+
+        return completedWorkouts
+    }
+    async getNoCompletedWorkouts(isCompleted: boolean): Promise<Workout[]> {
+        const noCompletedWorkouts = await prisma.workout.findMany({
+            where: {
+                isCompleted: false
+            }
+        })
+        
+        return noCompletedWorkouts
+    }
+}
