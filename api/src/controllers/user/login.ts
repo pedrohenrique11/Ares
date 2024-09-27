fa88172d0ec8c81b6d3b19cdba8e2270b460e083
Pedro Henrique <pedrohenriqueb.dev@gmail.com>
2024-09-26 22:38:39 -0300

    refactor(api): create z object

--- api/src/controllers/user/login.ts
+++ api/src/controllers/user/login.ts
@@ -2,12 +2,17 @@ import { Request, Response } from "express";
 import { PrismaUserRository } from "../../repositories/prisma/prisma-user-repository";
 import { LoginService } from "../../services/user/login";
 import { InvalidatedCredentials } from "../../services/errors/invalidated-credentials";
+import { z } from "zod";
 
 const userPrismaRository = new PrismaUserRository
 const loginService = new LoginService(userPrismaRository)
 
 export async function login(req: Request, res: Response) {
-    const {email, password} = req.body
+    const loginData = z.object({
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+    const {email, password} = loginData.parse(req.body)
 
     try {
         const token = await loginService.execute({email, password})
